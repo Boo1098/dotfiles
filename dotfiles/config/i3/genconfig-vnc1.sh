d03e09dea2c0a9b6f15a9003be6eeb2ff7e754d4
Boo1098 <nathansd1098@gmail.com>
2021-03-31 21:45:39 -0400

    Restructed i3 config to be editable (also stripped it down to be more utilitarian)

---
+++ dotfiles/config/i3/genconfig-vnc1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cat $HOME/.config/i3/config.base \
+        $HOME/.config/i3/config.i3bar \
+        $HOME/.config/i3/config.mod_key1 > $HOME/.config/i3/config
+echo "bindsym \$mod+Shift+r exec $HOME/.config/i3/genconfig-vnc1.sh && i3-msg restart" >> $HOME/.config/i3/config
