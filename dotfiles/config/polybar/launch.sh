56642a051df0bfeaf8151023c0838b8eda0bd9db
Boo1098 <nathansd1098@gmail.com>
2021-03-31 23:31:18 -0400

    switched to mpd revamped polybar

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -7,9 +7,11 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	MONITOR=$m polybar --reload top &
-done
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#	MONITOR=$m polybar --reload top &
+#done
 #polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+MONITOR="HDMI-0" polybar toptray 2>&1 | tee -a /tmp/polybar1.log & disown
+MONITOR="DVI-D-0" polybar topnotray 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
