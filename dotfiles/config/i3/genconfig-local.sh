d528d0f3f456906be669c6c54b05d42cb24a1005
Boo1098 <nathansd1098@gmail.com>
2021-04-19 22:50:38 -0400

    fixed up autostart programs

--- dotfiles/config/i3/genconfig-local.sh
+++ dotfiles/config/i3/genconfig-local.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cat $HOME/.config/i3/config.base \
-        $HOME/.config/i3/config.default_apps \
+cat  $HOME/.config/i3/config.mod_key1 \
+        $HOME/.config/i3/config.base \
+        $HOME/.config/i3/config.local \
         $HOME/.config/i3/config.polybar \
-        $HOME/.config/i3/config.mod_key1 > $HOME/.config/i3/config
+        $HOME/.config/i3/config.default_apps > $HOME/.config/i3/config
 echo "bindsym \$mod+Shift+r exec $HOME/.config/i3/genconfig-local.sh && i3-msg restart" >> $HOME/.config/i3/config
--- dotfiles/config/i3/genconfig-vnc2.sh
+++ dotfiles/config/i3/genconfig-vnc2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat $HOME/.config/i3/config.base \
-        $HOME/.config/i3/config.i3bar \
-        $HOME/.config/i3/config.mod_key1 > $HOME/.config/i3/config
+cat $HOME/.config/i3/config.mod_key4 \
+        $HOME/.config/i3/config.base \
+        $HOME/.config/i3/config.i3bar > $HOME/.config/i3/config
 echo "bindsym \$mod+Shift+r exec $HOME/.config/i3/genconfig-vnc2.sh && i3-msg restart" >> $HOME/.config/i3/config
