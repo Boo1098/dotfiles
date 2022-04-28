4cd49b978af0af946384d01ab573b50a409b6762
Boo1098 <nathansd1098@gmail.com>
2022-04-27 22:32:05 -0400

    Complete rewrite for NovaV2

--- dotfiles/config/i3/genconfig-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cat  $HOME/.config/i3/config.mod_key1 \
-        $HOME/.config/i3/config.base \
-        $HOME/.config/i3/config.local \
-        $HOME/.config/i3/config.polybar \
-        $HOME/.config/i3/config.default_apps > $HOME/.config/i3/config
-echo "bindsym \$mod+Shift+r exec $HOME/.config/i3/genconfig-local.sh && i3-msg restart" >> $HOME/.config/i3/config
--- dotfiles/config/i3/genconfig-vnc2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cat $HOME/.config/i3/config.mod_key4 \
-        $HOME/.config/i3/config.base \
-        $HOME/.config/i3/config.i3bar > $HOME/.config/i3/config
-echo "bindsym \$mod+Shift+r exec $HOME/.config/i3/genconfig-vnc2.sh && i3-msg restart" >> $HOME/.config/i3/config
